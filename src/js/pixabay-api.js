24881fde59d86e6e11d4f18542f2b1d1e04b2f28
AlexP <alexperepelyuk@gmail.com>
2025-02-12 12:23:59 +0100

    added script and styles

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,14 @@
 import axios from 'axios';
 
-const options = {
-  method: 'GET',
-};
-
-fetch('https://pixabay.com/api/', options)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .then(data => {
-    // Data handling
-  })
-  .catch(error => {
-    // Error handling
-  });
+export default function fetchImg(searchRequest) {
+  const options = {
+    params: {
+      key: '48773957-0ff53b0d4b6397cb99259d9c5',
+      q: searchRequest,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  };
+  return axios.get('https://pixabay.com/api/', options);
+}
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,94 +2,66 @@ import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const images = [
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-    description: 'Hokkaido Flower',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
-  },
-];
+import iziToast from 'izitoast';
+
+import 'izitoast/dist/css/iziToast.min.css';
+
+import icon from '../img/bi_x-octagon.svg';
 
 const gallery = document.querySelector('.gallery');
 
-function imageTemplate(image) {
-  return `<li class="gallery-item">
-                <a class="gallery-link" href="${image.original}">
-                <img
-                    class="gallery-image"
-                    src="${image.preview}"
-                    alt="${image.description}"
-                />
-                </a>
-            </li>`;
-}
+export function imagesTemplate(images) {
+  const galleryMarkup = images
+    .map(
+      item => `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${item.largeImageURL}">
+        <img class="gallery-image" src="${item.webformatURL}" alt="${item.tags}" />
+        <figcaption class="thumb-data">
+          <dl class="thumb-data-list">
+            <div class="thumb-data-item">
+              <dt class="thumb-data-title">Likes</dt>
+              <dd class="thumb-data-data">${item.likes}</dd>
+            </div>
+            <div class="thumb-data-item">
+              <dt class="thumb-data-title">Views</dt>
+              <dd class="thumb-data-data">${item.views}</dd>
+            </div>
+            <div class="thumb-data-item">
+              <dt class="thumb-data-title">Comments</dt>
+              <dd class="thumb-data-data">${item.comments}</dd>
+            </div>
+            <div class="thumb-data-item">
+              <dt class="thumb-data-title">Downloads</dt>
+              <dd class="thumb-data-data">${item.downloads}</dd>
+            </div>
+          </dl>
+        </figcaption>
+      </a>
+    </li>
+  `
+    )
+    .join('');
 
-function imagesTemplate(images) {
-  return images.map(imageTemplate).join('');
+  gallery.innerHTML = galleryMarkup;
+  lightbox.refresh();
 }
-
-gallery.innerHTML = imagesTemplate(images);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionPosition: 'bottom',
   captionDelay: 250,
 });
+
+export function showMessage() {
+  iziToast.show({
+    position: 'topRight',
+    message:
+      'Sorry, there are no images matching your search query. Please try again!',
+    messageColor: '#FFFFFF',
+    messageSize: '16px',
+    backgroundColor: '#EF4040',
+    maxWidth: '432px',
+    iconUrl: icon,
+  });
+}
